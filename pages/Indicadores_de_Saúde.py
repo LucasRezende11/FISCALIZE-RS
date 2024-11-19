cae1c0030c9a06afb2771a91a6aad58a970aa731
Lucas Alexandre Rezende <124625228+LucasRezende11@users.noreply.github.com>
2024-11-19 12:32:05 -0300

    Update Indicadores_de_Saúde.py

--- pages/Indicadores_de_Saúde.py
+++ pages/Indicadores_de_Saúde.py
@@ -203,7 +203,8 @@ def health_indicators():
 
         fig_asps.add_hline(y=15, line=dict(color='red', dash='dash', width=2), annotation_text="Mínimo 15%", annotation_position="bottom right")
 
-        fig_asps.update_layout(title=dict(text=f"Índice ASPS - {city.title()} vs média dos municípios do RS", font=dict(size=20)),
+        city = format_city_name(city)
+        fig_asps.update_layout(title=dict(text=f"Índice ASPS - {city} vs média dos municípios do RS", font=dict(size=20)),
             yaxis=dict(range=[0, max_asps], title='Índice ASPS (%)', titlefont=dict(size=14)), xaxis_title=None,
             xaxis_title_font=dict(size=14), dragmode=False, legend=dict(title=None, font=dict(size=12), orientation="h", y=-0.2, x=0.5, xanchor='center'),
             margin=dict(l=10, r=10, t=40, b=5))
